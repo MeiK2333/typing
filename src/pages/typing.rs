38590577196fab91e30503abcb09ed361a3b97a2
MeiK <meik2333@gmail.com>
2022-01-20 16:08:52 +0800

    完成打字功能

--- src/pages/typing.rs
+++ src/pages/typing.rs
@@ -1,11 +1,26 @@
+use std::{
+    fmt::{Debug},
+};
+use serde::{Deserialize, Serialize};
 use gloo::console;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlElement, HtmlInputElement};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{HtmlElement, HtmlInputElement, Request, RequestInit, RequestMode, Response};
 use yew::prelude::*;
 use crate::components::{typing_line::TypingLine};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Post {
+    pub title: String,
+    pub author: String,
+    pub lines: Vec<String>,
+}
+
+
 pub enum Msg {
     Update,
+    SetPost(JsValue),
     LineDone(usize, Vec<char>),
 }
 
@@ -21,23 +36,51 @@ pub struct Typing {
     spans: Vec<char>,
     lines: Vec<LineState>,
     active_line: usize,
+    loading: bool,
+    load_msg: String,
 }
 
 impl Component for Typing {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
+    fn create(ctx: &Context<Self>) -> Self {
+        ctx.link().send_future(async {
+            let url = "https://poetrydb.org/random";
+            let mut opts = RequestInit::new();
+            opts.method("GET");
+            opts.mode(RequestMode::Cors);
+            let request = Request::new_with_str_and_init(url, &opts).unwrap();
+            let window = web_sys::window().unwrap();
+            let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.unwrap();
+            let resp: Response = resp_value.dyn_into().unwrap();
+            Msg::SetPost(JsFuture::from(resp.json().unwrap()).await.unwrap())
+        });
         Self {
-            spans: include_str!("../data/1.txt").chars().collect(),
+            spans: vec![],
             lines: vec![],
             active_line: 0,
+            loading: true,
+            load_msg: "获取文章中".to_string(),
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Update => {
+                true
+            }
+            Msg::SetPost(js_value) => {
+                let mut posts: Vec<Post> = js_value.into_serde().unwrap();
+                let post = posts.get_mut(0).unwrap();
+                self.lines.clear();
+                for line in &post.lines {
+                    for char in line.chars() {
+                        self.spans.push(char);
+                    }
+                    self.spans.push('\n');
+                }
+                self.load_msg = "加载文章中".to_string();
                 true
             }
             Msg::LineDone(line, value) => {
@@ -63,21 +106,36 @@ impl Component for Typing {
         return html! {
 <>
 {
-    for self.lines.iter().enumerate().map(|(idx, line)| {
+    if !self.loading {
         html! {
-            <TypingLine
-                key={idx}
-                ref={line.node_ref.clone()}
-                disabled={line.disabled}
-                line_number={line.line_number}
-                line={line.text.clone()}
-                on_done={on_done.clone()}
-            ></TypingLine>
+            {
+                for self.lines.iter().enumerate().map(|(idx, line)| {
+                    html! {
+                        <TypingLine
+                            key={idx}
+                            ref={line.node_ref.clone()}
+                            disabled={line.disabled}
+                            line_number={line.line_number}
+                            line={line.text.clone()}
+                            on_done={on_done.clone()}
+                        ></TypingLine>
+                    }
+                })
+            }
         }
-    })
+    } else {
+        html! {
+            <div class="d-flex justify-content-center">
+                <strong>{ self.load_msg.clone() }</strong>
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">{ "Loading..." }</span>
+                </div>
+            </div>
+        }
+    }
 }
 {
-    if self.lines.len() == 0 {
+    if self.spans.len() != 0 {
         html!{
             <div class="card typing-hidden" style="padding: 18px;">
                 <div class="mb-3 typing-form typing-hidden">
@@ -103,56 +161,61 @@ impl Component for Typing {
         };
     }
 
-    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
-        if !first_render {
-            console::debug!(format!("focus line {}", self.active_line));
-            self.lines.get_mut(self.active_line).unwrap()
-                .node_ref.cast::<HtmlElement>().unwrap()
-                .query_selector("input").unwrap().unwrap()
-                .dyn_into::<HtmlInputElement>().unwrap()
-                .focus().unwrap();
+    fn rendered(&mut self, ctx: &Context<Self>, _first_render: bool) {
+        if self.loading == false {
+            if self.lines.len() > 0 {
+                console::debug!(format!("focus line {}", self.active_line));
+                self.lines.get_mut(self.active_line).unwrap()
+                    .node_ref.cast::<HtmlElement>().unwrap()
+                    .query_selector("input").unwrap().unwrap()
+                    .dyn_into::<HtmlInputElement>().unwrap()
+                    .focus().unwrap();
+            }
             return;
         }
-        let document = web_sys::window().unwrap().document().unwrap();
+        if self.spans.len() != 0 {
+            let document = web_sys::window().unwrap().document().unwrap();
 
-        let spans = document
-            .query_selector_all("#typing-hidden > span")
-            .unwrap()
-            .dyn_into::<web_sys::NodeList>().unwrap();
+            // 获取文章在页面上渲染的结果
+            let spans = document
+                .query_selector_all("#typing-hidden > span")
+                .unwrap()
+                .dyn_into::<web_sys::NodeList>().unwrap();
 
-        self.lines.clear();
-
-        // TODO: 此处需要单独抽离出来作为模块
-        let mut offset_top = 0;
-        let mut pos = 0;
-        for idx in 0..spans.length() {
-            let elem = spans.item(idx).unwrap().dyn_into::<HtmlElement>().unwrap();
-            if elem.offset_top() != offset_top {
-                if offset_top != 0 {
-                    let mut left = pos;
-                    let mut right = idx as usize;
-                    while self.spans[left].is_whitespace() {
-                        left += 1;
+            let mut offset_top = 0;
+            let mut pos = 0;
+            for idx in 0..spans.length() {
+                let elem = spans.item(idx).unwrap().dyn_into::<HtmlElement>().unwrap();
+                // 对每个字符判断是否发生了换行
+                if elem.offset_top() != offset_top {
+                    if offset_top != 0 {
+                        let mut left = pos;
+                        let mut right = idx as usize;
+                        // 忽略两端空白符号
+                        while right > left && self.spans[left].is_whitespace() {
+                            left += 1;
+                        }
+                        while right > left && self.spans[right - 1].is_whitespace() {
+                            right -= 1;
+                        }
+                        let line = self.spans[left as usize..right as usize].to_vec();
+                        pos = idx as usize;
+                        if line.clone().into_iter().any(|x| x != '\n') {
+                            self.lines.push(LineState {
+                                text: line,
+                                disabled: self.lines.len() != 0,
+                                node_ref: Default::default(),
+                                line_number: self.lines.len(),
+                                value: vec![],
+                            });
+                        }
                     }
-                    while right > left && self.spans[right - 1].is_whitespace() {
-                        right -= 1;
-                    }
-                    let line = self.spans[left as usize..right as usize].to_vec();
-                    pos = idx as usize;
-                    if line.clone().into_iter().any(|x| x != '\n') {
-                        self.lines.push(LineState {
-                            text: line,
-                            disabled: self.lines.len() != 0,
-                            node_ref: Default::default(),
-                            line_number: self.lines.len(),
-                            value: vec![],
-                        });
-                    }
+                    offset_top = elem.offset_top();
                 }
-                offset_top = elem.offset_top();
             }
+            self.spans.clear();
+            self.loading = false;
+            ctx.link().send_message(Msg::Update);
         }
-
-        ctx.link().send_message(Msg::Update);
     }
 }
